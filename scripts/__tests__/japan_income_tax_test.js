a82341495eb856b577659aa0af47991ad59fac85
Yas Okada <yas.okadatech@gmail.com>
2015-12-25 07:45:09 +0900

    テスト追加：住民税、ふるさと納税上限

--- scripts/__tests__/japan_income_tax_test.js
+++ scripts/__tests__/japan_income_tax_test.js
@@ -1,9 +1,9 @@
 //jest.dontMock('../japan_income_tax');
 jest.autoMockOff();
 
-let { japan_income_tax } = require('../japan_income_tax.js');
 
 describe('income tax', () => {
+  let { japan_income_tax } = require('../japan_income_tax.js');
 
   it('should calculate tax', () => {
     let data = {
@@ -31,6 +31,64 @@ describe('income tax', () => {
 
 });
 
+describe('resident tax', () => {
+  let { japan_resident_tax } = require('../japan_income_tax.js');
+
+  it('should calculate tax', () => {
+    let data = {
+      income: 5000000,
+      spouse: false,
+      children16: 0,
+      children19: 0
+    };
+    let tax = japan_resident_tax(data);
+
+    expect(tax).toEqual(315500);
+  });
+
+  it('should calculate tax with deduction', () => {
+    let data = {
+      income: 5000000,
+      spouse: true,
+      children16: 0,
+      children19: 0
+    };
+    let tax = japan_resident_tax(data);
+
+    expect(tax).toEqual(282500);
+  });
+
+});
+
+describe('furusato max', () => {
+  let { furusato_tax_max } = require('../japan_income_tax.js');
+
+  it('should calculate', () => {
+    let data = {
+      income: 5000000,
+      spouse: false,
+      children16: 0,
+      children19: 0
+    };
+    let max = furusato_tax_max(data);
+
+    expect(max).toEqual(80800);
+  });
+
+  it('should calculate it with deduction', () => {
+    let data = {
+      income: 5000000,
+      spouse: true,
+      children16: 0,
+      children19: 0
+    };
+    let max = furusato_tax_max(data);
+
+    expect(max).toEqual(72600);
+  });
+
+});
+
 describe ('floor1000', () => {
   let { floor1000 } = require('../japan_income_tax.js');
 
@@ -42,6 +100,21 @@ describe ('floor1000', () => {
     expect(floor1000(1001)).toEqual(1000);
     expect(floor1000(1999)).toEqual(1000);
     expect(floor1000(2001)).toEqual(2000);
+  });
+
+});
+
+describe ('floor100', () => {
+  let { floor100 } = require('../japan_income_tax.js');
+
+  it('should calculate', () => {
+
+    expect(floor100(1)).toEqual(0);
+    expect(floor100(99)).toEqual(0);
+    expect(floor100(100)).toEqual(100);
+    expect(floor100(101)).toEqual(100);
+    expect(floor100(199)).toEqual(100);
+    expect(floor100(201)).toEqual(200);
   });
 
 });
